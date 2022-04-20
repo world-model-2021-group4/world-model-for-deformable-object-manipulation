c02c5195bf9b89e75f556384251b2e91d0fd3888
Kanazawanaoaki <kanazawa@jsk.imi.i.u-tokyo.ac.jp>
2022-04-20 16:42:27 +0900

    Add title

--- scripts/exec_plot.bash
+++ scripts/exec_plot.bash
@@ -1,5 +1,7 @@
 for file in `\find data -name '*.csv'`; do
-    echo "$file"
-    fig_name=${file%%.*}.png
-    python progress_to_plot.py -d ${file} -f ${fig_name}
+    before_period=${file%%.*}
+    echo "$before_period"
+    fig_name=${before_period}.png
+    title=${before_period#*/}
+    python3 progress_to_plot.py -d ${file} -f ${fig_name} -t ${title}
 done
--- scripts/progress_to_plot.py
+++ scripts/progress_to_plot.py
@@ -2,14 +2,14 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
-def progress_to_plat(csv_path, fig_path):
+def progress_to_plat(csv_path, fig_path, title):
     df = pd.read_csv(csv_path)
     # fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(9, 7))
     # df[['num_episodes', 'train_rewards']].plot(x='num_episodes', ax=axes[0, 0])
     # df[['num_episodes', 'observation_loss']].plot(x='num_episodes', ax=axes[0, 1], logy=True)
     # df[['num_episodes', 'kl_loss']].plot(x='num_episodes', ax=axes[1, 0])
     # df[['num_episodes', 'reward_loss']].plot(x='num_episodes', ax=axes[1, 1], logy=True)
-    df[['num_episodes', 'train_rewards', 'observation_loss', 'kl_loss', 'reward_loss']].plot.line(x='num_episodes', subplots=True, layout=(2, 2), figsize=(9, 6))
+    df[['num_episodes', 'train_rewards', 'observation_loss', 'kl_loss', 'reward_loss']].plot.line(title=title, x='num_episodes', subplots=True, layout=(2, 2), figsize=(9, 6))
     # plt.show()
     plt.savefig(fig_path)
     plt.close('all')
@@ -18,7 +18,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--data', default='/data/progress.csv', type=str)
     parser.add_argument('-f', '--fig', default='/data/plot.png', type=str)
+    parser.add_argument('-t', '--title', default='FoldCloth', type=str)
     args = parser.parse_args()
     csv_path = args.data
     fig_path = args.fig
-    progress_to_plat(csv_path, fig_path)
+    title = args.title
+    progress_to_plat(csv_path, fig_path, title)
